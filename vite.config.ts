80a3fcb57ca3b30b19259d2de68e7b773643e9fb
bkataru <baalateja.k@gmail.com>
2025-10-05 14:29:02 +0530

    checkpoint: added tsconfig.json tsconfig.node.json vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import fs from "node:fs";
+
+import preact from "@preact/preset-vite";
+import tailwindcss from "@tailwindcss/vite";
+import dotenv from "dotenv";
+import { defineConfig } from "vite";
+import htmlPlugin from "vite-plugin-html-config";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+dotenv.config();
+
+const TITLE = "Jarvis - your private, personal assistant";
+
+export default defineConfig({
+	worker: {
+		format: "es",
+	},
+	server: {
+		...(fs.existsSync(process.env.SSL_KEY || "") &&
+		fs.existsSync(process.env.SSL_CRT || "")
+			? {
+					https: {
+						key: fs.readFileSync(process.env.SSL_KEY || ""),
+						cert: fs.readFileSync(process.env.SSL_CRT || ""),
+					},
+				}
+			: {}),
+		port: process.env.PORT ? parseInt(process.env.PORT, 10) : 8080,
+	},
+	plugins: [
+		preact(),
+		tsconfigPaths(),
+		tailwindcss(),
+		htmlPlugin({ title: TITLE }),
+	],
+});
